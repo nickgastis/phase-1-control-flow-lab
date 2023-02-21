ca573e33b6cb96a423f03ea0a46f383d72e51ae0
Nick Gastis <nickgastis1@gmail.com>
2023-02-21 15:44:35 -0500

    done

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(distance) {
+
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  const message = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+function switchOnCharmFromTip(tipAmount){
+  let message;
+  switch (tipAmount) {
+    case "generous":
+      message = "Thank you so much.";
+      break;
+      case "not as generous":
+        message = "Thank you."
+        break;
+        default:
+          message = "Bye."
+  }
+  return message
 }
